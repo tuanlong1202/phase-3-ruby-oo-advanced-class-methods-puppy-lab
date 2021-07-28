b45a4f02efd421e567acac6d2f992b7b5548836c
tuanlong1202 <tuanlong1202@gmail.com>
2021-07-27 23:01:11 -0400

    phase 3 ruby oo advanced class methods puppy lab

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,35 @@
 # Add your code here
+class Dog
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def name
+        @name
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def self.print_all
+        strRet = ""
+        @@all.each do |dog|
+            strRet += "#{dog.name}\n"
+        end
+        puts strRet
+    end
+
+end
